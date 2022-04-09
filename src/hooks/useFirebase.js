10c310643310a9abcc560b57f723656832c932f4
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-09 22:11:56 +0600

    new code

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,49 +1,49 @@
-// import { useState } from "react";
-// import app from "../firebase.init";
-// import {
-//   getAuth,
-//   GoogleAuthProvider,
-//   onAuthStateChanged,
-//   signInWithPopup,
-//   signOut,
-// } from "firebase/auth";
-// import { useEffect } from "react";
+import { useState } from "react";
+import app from "../firebase.init";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useEffect } from "react";
 
-// const auth = getAuth(app);
-// const googleProvider = new GoogleAuthProvider();
-// const useFirebase = () => {
-//   const [user, setUser] = useState({});
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const useFirebase = () => {
+  const [user, setUser] = useState({});
 
-//   const signInWithGoogle = () => {
-//     signInWithPopup(auth, googleProvider)
-//       .then((result) => {
-//         const user = result.user;
-//         setUser(user);
-//         console.log(user);
-//       })
-//       .catch((error) => {
-//         console.log(error, error.message);
-//       });
-//   };
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error, error.message);
+      });
+  };
 
-//   const handleSignOut = () => {
-//        signOut(auth)
-//        .then(() =>{
+  const handleSignOut = () => {
+       signOut(auth)
+       .then(() =>{
 
-//        })
-//   }
+       })
+  }
 
-//   useEffect(() => {
-//     onAuthStateChanged(auth, (user) => {
-//       setUser(user);
-//     });
-//   }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, []);
 
-//   return {
-//     user,
-//     handleSignOut,
-//     signInWithGoogle: signInWithGoogle,
-//   };
-// };
+  return {
+    user,
+    handleSignOut,
+    signInWithGoogle: signInWithGoogle,
+  };
+};
 
-// export default useFirebase;
+export default useFirebase;
