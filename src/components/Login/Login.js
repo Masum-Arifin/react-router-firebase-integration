c76c1ae18b42edb50b02ab41bf8f429ae116873d
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-09 14:52:40 +0600

    new code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../hooks/useFirebase";
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import app from "../../firebase.init";
+
+const auth = getAuth(app)
 
 const Login = () => {
-    const {signInWithGoogle} = useFirebase(); 
+   const [signInWithGoogle] = useSignInWithGoogle(auth)
   return (
     <div>
       <h3>Please Login</h3>
       <div style={{margin:'20px', color:'blue'}}>
-      <button style={{margin:'2px', color:'white',backgroundColor:'green', padding:'10px', fontWeight:'700', borderRadius:'10px'}} onClick={signInWithGoogle}>Google Sign In</button>
+      <button style={{margin:'2px', color:'white',backgroundColor:'green', padding:'10px', fontWeight:'700', borderRadius:'10px'}} onClick={   () => signInWithGoogle()}>Google Sign In</button>
       </div>
       
       <form>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
 
+const auth = getAuth(app);
 const Products = () => {
-    const { user} = useFirebase();
-    return (
-        <div>
-            <h2>Knock Knock ! who is there</h2>
-            <h5>{user ? user.displayName : 'vooooot'}</h5>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <h2>Knock Knock ! who is there</h2>
+      <h5>{user ? user.displayName : "vooooot"}</h5>
+    </div>
+  );
 };
 
 export default Products;
