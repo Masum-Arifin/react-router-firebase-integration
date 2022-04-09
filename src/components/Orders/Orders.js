97b492ec12ac77096036f2a90a7ebfef2aa73175
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-09 22:06:23 +0600

    new code

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const Orders = () => {
-    return (
-        <div>
-            <h2>This is Orders</h2>
-        </div>
-    );
-};
+// const Orders = () => {
+//     return (
+//         <div>
+//             <h2>This is Orders</h2>
+//         </div>
+//     );
+// };
 
-export default Orders;
+// export default Orders;
