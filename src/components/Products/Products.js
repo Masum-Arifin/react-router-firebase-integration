824493dcda81c96efe6560006c96cee07b4617d1
Masum-Arifin <masumarifinbd@gmail.com>
2022-04-09 22:10:40 +0600

    new code

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,17 +1,18 @@
-import { getAuth } from "firebase/auth";
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import app from "../../firebase.init";
+// import { getAuth } from 'firebase/auth';
+// import React from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import app from '../../firebase.init';
 
-const auth = getAuth(app);
-const Products = () => {
-  const [user] = useAuthState(auth);
-  return (
-    <div>
-      <h2>Knock Knock ! who is there</h2>
-      <h5>{user ? user.displayName : "vooooot"}</h5>
-    </div>
-  );
-};
+// const auth = getAuth(app)
+// const Products = () => {
+    
+//     const [user] = useAuthState(auth)
+//     return (
+//         <div>
+//             <h2>Knock Knock ! who is there</h2>
+//             <h5>{user ? user.displayName : 'vooooot'}</h5>
+//         </div>
+//     );
+// };
 
-export default Products;
+// export default Products;
